2cd3d0afeb395f17613ba094fd2edf1740f2b09c
kayerosaint <kayerosaint@gmail.com>
2022-09-02 22:30:28 -0700

    add outputs

--- output.tf
+++ output.tf
@@ -4,6 +4,26 @@ output "public_subnet_ids" {
   value = aws_subnet.public_subnets[*].id
 }
 
-output "web_server_public_ip" {
-  value = aws_instance.app_server.public_ip
+output "web_server_public_ips" {
+  value = aws_instance.app_server[*].public_ip
 }
+
+output "vpc_cidr_block" {
+  value = try(data.aws_vpc.dev_vpc[*].cidr_block, "")
+}
+
+output "my_static_ips" {
+  value = aws_eip.eip[*].public_ip
+}
+
+
+
+
+
+
+
+/*
+output "public_subnet_ids" {
+  value = aws_subnet.public_subnets[*].id
+}
+*/
